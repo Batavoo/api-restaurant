baa94d202744777588d584df5969ef2c062c0ac8
Gustavo Santos Arruda <gsarruda.office@gmail.com>
2025-08-20 16:43:51 -0300

    implement tables and tables sessions functionality with CRUD operations and seed data

---
+++ src/controllers/tables-controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "@/utils/AppError";
+import { z } from "zod";
+import { knex } from "@/database/knex";
+
+class TablesController {
+  async index(request: Request, response: Response, next: NextFunction) {
+    try {
+      const tables = await knex<tableRepository>("tables")
+        .select()
+        .orderBy("table_number");
+
+      return response.json(tables);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export { TablesController };
