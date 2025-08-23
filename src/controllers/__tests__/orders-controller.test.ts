01f664022e3532252c1fc16ad8704e1df5f3cb7b
Gustavo Santos Arruda <88382736+Batavoo@users.noreply.github.com>
2025-08-23 09:55:36 -0300

    merge develop into main (#8)
    * 🔧 feat: add lint and unit tests (#2)
    
    * 🔧 feat: add lint and unit tests
    
    * 🔥 chore: remove package-lock.json
    
    * 🙈 chore: ignore package lock
    
    * feat:add github ci file
    
    * Feature/add unit tests (#3)
    
    * 🔧 feat: add lint and unit tests
    
    * 🔥 chore: remove package-lock.json
    
    * 🙈 chore: ignore package lock
    
    * 🧪 chore: update tests
    
    * 🧪 chore: rename tests
    
    ---------
    
    Co-authored-by: Victor Neves <43397591+v-venes@users.noreply.github.com>
    Co-authored-by: RenanRoseno <rosenorenan@gmail.com>

---
+++ src/controllers/__tests__/orders-controller.test.ts
@@ -0,0 +1,77 @@
+import { Request, Response, NextFunction } from 'express';
+import { OrdersController } from '@/controllers/orders-controller';
+import { AppError } from '@/utils/AppError';
+
+const knexMock: Record<string, jest.Mock> = {
+  where: jest.fn().mockReturnThis(),
+  first: jest.fn(),
+  select: jest.fn().mockReturnThis(),
+  join: jest.fn().mockReturnThis(),
+  orderBy: jest.fn().mockReturnThis(),
+  insert: jest.fn(),
+  raw: jest.fn(),
+};
+
+jest.mock('../database/knex', () => {
+  return {
+    knex: jest.fn(() => knexMock),
+  };
+});
+
+describe('OrdersController - create', () => {
+  let ordersController: OrdersController;
+  let mockResponse: Partial<Response>;
+  let mockNext: jest.Mock;
+
+  beforeEach(() => {
+    ordersController = new OrdersController();
+
+    mockResponse = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    mockNext = jest.fn();
+    jest.clearAllMocks();
+  });
+
+  it('should throw error if session not exists', async () => {
+    knexMock.first.mockResolvedValueOnce(undefined);
+    const mockRequest = {
+      body: { table_session_id: 1, product_id: 1, quantity: 2 },
+    } as Request;
+
+    await ordersController.create(
+      mockRequest,
+      mockResponse as Response,
+      mockNext as NextFunction,
+    );
+
+    expect(mockNext).toHaveBeenCalledWith(expect.any(AppError));
+    expect((mockNext.mock.calls[0][0] as AppError).message).toBe(
+      'session table not found',
+    );
+  });
+
+  it('should create order', async () => {
+    knexMock.first
+      .mockResolvedValueOnce({ id: 1, closed_at: null })
+      .mockResolvedValueOnce({ id: 1, price: 10 });
+
+    knexMock.insert.mockResolvedValueOnce([1]);
+
+    const mockRequest = {
+      body: { table_session_id: 1, product_id: 1, quantity: 2 },
+    } as Request;
+
+    await ordersController.create(
+      mockRequest,
+      mockResponse as Response,
+      mockNext as NextFunction,
+    );
+
+    expect(mockResponse.status).toHaveBeenCalledWith(201);
+    expect(mockResponse.json).toHaveBeenCalledWith();
+    expect(mockNext).not.toHaveBeenCalled();
+  });
+});
