decba9614d716f417947e9bda53c613a8c6ee939
Victor Neves <43397591+v-venes@users.noreply.github.com>
2025-08-24 18:46:18 -0300

    💚 ci: configure sonar properties

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,13 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+  collectCoverageFrom: [
+    'src/**/*.test.ts',
+    '!**/node_modules/**',
+    '!**/dist/**',
+    '!**/coverage/**',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text', 'text-summary'],
+  testResultsProcessor: 'jest-sonar-reporter',
 };
